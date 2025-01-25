dfc1f825157e6e75309b21cd4d304a44301e4512
Yuxin Yang <yuxinyan@kth.se>
2025-01-25 23:54:45 +0100

    1.25 First Commit

---
+++ Tasks/Task_3/bin/avr-rss2/ztimer_core/periph_timer.d
@@ -0,0 +1,196 @@
+/home/yang0902/RIOT/examples/Task_3/bin/avr-rss2/ztimer_core/periph_timer.o: \
+ /home/yang0902/RIOT/sys/ztimer/periph_timer.c \
+ /home/yang0902/RIOT/examples/Task_3/bin/avr-rss2/riotbuild/riotbuild.h \
+ /home/yang0902/RIOT/core/lib/include/assert.h \
+ /usr/lib/gcc/avr/7.3.0/include/stdint.h /usr/lib/avr/include/stdint.h \
+ /home/yang0902/RIOT/core/lib/include/irq.h \
+ /usr/lib/gcc/avr/7.3.0/include/stdbool.h \
+ /home/yang0902/RIOT/cpu/atmega_common/include/cpu_conf.h \
+ /home/yang0902/RIOT/cpu/atmega_common/include/atmega_regs_common.h \
+ /usr/lib/avr/include/avr/io.h /usr/lib/avr/include/avr/sfr_defs.h \
+ /home/yang0902/RIOT/cpu/avr8_common/avr_libc_extra/include/inttypes.h \
+ /usr/lib/avr/include/inttypes.h /usr/lib/avr/include/avr/iom256rfr2.h \
+ /usr/lib/avr/include/avr/portpins.h /usr/lib/avr/include/avr/common.h \
+ /usr/lib/avr/include/avr/version.h /usr/lib/avr/include/avr/fuse.h \
+ /usr/lib/avr/include/avr/lock.h /usr/lib/avr/include/avr/power.h \
+ /home/yang0902/RIOT/cpu/avr8_common/include/irq_arch.h \
+ /home/yang0902/RIOT/cpu/avr8_common/include/stdio.h \
+ /usr/lib/avr/include/stdio.h /usr/lib/gcc/avr/7.3.0/include/stdarg.h \
+ /usr/lib/gcc/avr/7.3.0/include/stddef.h \
+ /home/yang0902/RIOT/sys/include/flash_utils.h \
+ /usr/lib/avr/include/string.h \
+ /home/yang0902/RIOT/core/lib/include/kernel_defines.h \
+ /home/yang0902/RIOT/core/lib/include/modules.h \
+ /home/yang0902/RIOT/core/lib/include/riot_version.h \
+ /home/yang0902/RIOT/core/lib/include/compiler_hints.h \
+ /home/yang0902/RIOT/core/lib/include/container.h \
+ /home/yang0902/RIOT/cpu/avr8_common/include/flash_utils_arch.h \
+ /usr/lib/avr/include/avr/pgmspace.h \
+ /home/yang0902/RIOT/cpu/avr8_common/include/cpu.h \
+ /usr/lib/avr/include/avr/interrupt.h \
+ /home/yang0902/RIOT/sys/include/architecture.h \
+ /usr/lib/gcc/avr/7.3.0/include-fixed/limits.h \
+ /home/yang0902/RIOT/cpu/avr8_common/include/architecture_arch.h \
+ /home/yang0902/RIOT/cpu/atmega_common/include/cpu_clock.h \
+ /home/yang0902/RIOT/core/include/sched.h \
+ /home/yang0902/RIOT/core/include/native_sched.h \
+ /home/yang0902/RIOT/core/lib/include/clist.h \
+ /home/yang0902/RIOT/core/lib/include/list.h \
+ /home/yang0902/RIOT/core/include/thread.h \
+ /home/yang0902/RIOT/core/lib/include/cib.h \
+ /home/yang0902/RIOT/core/lib/include/assert.h \
+ /home/yang0902/RIOT/core/include/msg.h \
+ /home/yang0902/RIOT/core/include/sched.h \
+ /home/yang0902/RIOT/core/lib/include/thread_config.h \
+ /home/yang0902/RIOT/cpu/avr8_common/include/thread_arch.h \
+ /home/yang0902/RIOT/cpu/avr8_common/include/states_internal.h \
+ /home/yang0902/RIOT/sys/include/ztimer/periph_timer.h \
+ /home/yang0902/RIOT/sys/include/ztimer.h \
+ /home/yang0902/RIOT/core/include/mbox.h \
+ /home/yang0902/RIOT/core/lib/include/list.h \
+ /home/yang0902/RIOT/core/include/msg.h \
+ /home/yang0902/RIOT/core/include/mutex.h \
+ /home/yang0902/RIOT/core/include/thread.h \
+ /home/yang0902/RIOT/core/lib/include/rmutex.h \
+ /home/yang0902/RIOT/drivers/include/periph/timer.h \
+ /home/yang0902/RIOT/cpu/atmega256rfr2/include/periph_cpu.h \
+ /home/yang0902/RIOT/cpu/atmega_common/include/periph_cpu_common.h \
+ /home/yang0902/RIOT/cpu/avr8_common/include/cpu.h \
+ /home/yang0902/RIOT/cpu/atmega_common/include/atmega_regs_common.h \
+ /home/yang0902/RIOT/boards/avr-rss2/include/periph_conf.h \
+ /home/yang0902/RIOT/boards/common/atmega/include/periph_conf_atmega_common.h \
+ /home/yang0902/RIOT/cpu/atmega256rfr2/include/default_timer_config.h
+
+/home/yang0902/RIOT/examples/Task_3/bin/avr-rss2/riotbuild/riotbuild.h:
+
+/home/yang0902/RIOT/core/lib/include/assert.h:
+
+/usr/lib/gcc/avr/7.3.0/include/stdint.h:
+
+/usr/lib/avr/include/stdint.h:
+
+/home/yang0902/RIOT/core/lib/include/irq.h:
+
+/usr/lib/gcc/avr/7.3.0/include/stdbool.h:
+
+/home/yang0902/RIOT/cpu/atmega_common/include/cpu_conf.h:
+
+/home/yang0902/RIOT/cpu/atmega_common/include/atmega_regs_common.h:
+
+/usr/lib/avr/include/avr/io.h:
+
+/usr/lib/avr/include/avr/sfr_defs.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/avr_libc_extra/include/inttypes.h:
+
+/usr/lib/avr/include/inttypes.h:
+
+/usr/lib/avr/include/avr/iom256rfr2.h:
+
+/usr/lib/avr/include/avr/portpins.h:
+
+/usr/lib/avr/include/avr/common.h:
+
+/usr/lib/avr/include/avr/version.h:
+
+/usr/lib/avr/include/avr/fuse.h:
+
+/usr/lib/avr/include/avr/lock.h:
+
+/usr/lib/avr/include/avr/power.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/include/irq_arch.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/include/stdio.h:
+
+/usr/lib/avr/include/stdio.h:
+
+/usr/lib/gcc/avr/7.3.0/include/stdarg.h:
+
+/usr/lib/gcc/avr/7.3.0/include/stddef.h:
+
+/home/yang0902/RIOT/sys/include/flash_utils.h:
+
+/usr/lib/avr/include/string.h:
+
+/home/yang0902/RIOT/core/lib/include/kernel_defines.h:
+
+/home/yang0902/RIOT/core/lib/include/modules.h:
+
+/home/yang0902/RIOT/core/lib/include/riot_version.h:
+
+/home/yang0902/RIOT/core/lib/include/compiler_hints.h:
+
+/home/yang0902/RIOT/core/lib/include/container.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/include/flash_utils_arch.h:
+
+/usr/lib/avr/include/avr/pgmspace.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/include/cpu.h:
+
+/usr/lib/avr/include/avr/interrupt.h:
+
+/home/yang0902/RIOT/sys/include/architecture.h:
+
+/usr/lib/gcc/avr/7.3.0/include-fixed/limits.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/include/architecture_arch.h:
+
+/home/yang0902/RIOT/cpu/atmega_common/include/cpu_clock.h:
+
+/home/yang0902/RIOT/core/include/sched.h:
+
+/home/yang0902/RIOT/core/include/native_sched.h:
+
+/home/yang0902/RIOT/core/lib/include/clist.h:
+
+/home/yang0902/RIOT/core/lib/include/list.h:
+
+/home/yang0902/RIOT/core/include/thread.h:
+
+/home/yang0902/RIOT/core/lib/include/cib.h:
+
+/home/yang0902/RIOT/core/lib/include/assert.h:
+
+/home/yang0902/RIOT/core/include/msg.h:
+
+/home/yang0902/RIOT/core/include/sched.h:
+
+/home/yang0902/RIOT/core/lib/include/thread_config.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/include/thread_arch.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/include/states_internal.h:
+
+/home/yang0902/RIOT/sys/include/ztimer/periph_timer.h:
+
+/home/yang0902/RIOT/sys/include/ztimer.h:
+
+/home/yang0902/RIOT/core/include/mbox.h:
+
+/home/yang0902/RIOT/core/lib/include/list.h:
+
+/home/yang0902/RIOT/core/include/msg.h:
+
+/home/yang0902/RIOT/core/include/mutex.h:
+
+/home/yang0902/RIOT/core/include/thread.h:
+
+/home/yang0902/RIOT/core/lib/include/rmutex.h:
+
+/home/yang0902/RIOT/drivers/include/periph/timer.h:
+
+/home/yang0902/RIOT/cpu/atmega256rfr2/include/periph_cpu.h:
+
+/home/yang0902/RIOT/cpu/atmega_common/include/periph_cpu_common.h:
+
+/home/yang0902/RIOT/cpu/avr8_common/include/cpu.h:
+
+/home/yang0902/RIOT/cpu/atmega_common/include/atmega_regs_common.h:
+
+/home/yang0902/RIOT/boards/avr-rss2/include/periph_conf.h:
+
+/home/yang0902/RIOT/boards/common/atmega/include/periph_conf_atmega_common.h:
+
+/home/yang0902/RIOT/cpu/atmega256rfr2/include/default_timer_config.h:
